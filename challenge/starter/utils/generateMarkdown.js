c39a7c2217e5cb227e0394e9de3e4994bedc018e
Hamdi <hamdishh2003@gmail.com>
2024-01-24 18:13:54 +0000

    editing the generateMarkdown.js errors in syntax

--- challenge/starter/utils/generateMarkdown.js
+++ challenge/starter/utils/generateMarkdown.js
@@ -2,6 +2,29 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Challenge Instructions
+${data.challenge}
+
+## Usage
+- Clone the repository USING SSH key.
+- Run the command \`npm install\` to install dependencies.
+- Run the command \`node index.js\` to generate the READ
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact [${data.username}](https://github.com/${data.username}) or email at ${data.email}.
+
 `;
 }
 
